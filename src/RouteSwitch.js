8b47eb53eb0f963723d5f9865cb31f74f5441133
Taaaaab <owen.whitsett@gmail.com>
2022-12-11 13:01:34 -0500

    Setup for deployment

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -5,7 +5,7 @@ import Shop from "./components/Shop";
 
 const RouteSwitch = () => {
     return (
-        <HashRouter>
+        <HashRouter basename='/shopping-cart'>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/shop" element={<Shop />} />
